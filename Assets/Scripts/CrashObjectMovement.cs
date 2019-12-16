a60f707b1725f5d1e0c1dfd8aaf3e90097daf9e6
Unknown <vermeulen.jimmy@gmail.com>
2019-12-16 07:56:00 +0100

    Add moving crash object

---
+++ Assets/Scripts/CrashObjectMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashObjectMovement : MonoBehaviour
+{
+    [SerializeField]
+    float startX;
+    [SerializeField]
+    float endX;
+    [SerializeField]
+    float speed;
+
+    bool movingToEnd = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingToEnd)
+        {
+            if(transform.position.x < endX)
+            {
+                transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                movingToEnd = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x > startX)
+            {
+                transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            }
+            else
+            {
+                movingToEnd = true;
+            }
+        }
+    }
+}
