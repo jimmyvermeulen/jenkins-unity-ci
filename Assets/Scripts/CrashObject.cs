15b846d85527275f720455a7fbdc72f4eb81fa3b
Unknown <vermeulen.jimmy@gmail.com>
2019-12-16 04:41:53 +0100

    Add stationary crash object

---
+++ Assets/Scripts/CrashObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashObject : MonoBehaviour
+{
+    public delegate void CrashEvent();
+    public static event CrashEvent OnCrashedInto;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnCrashedInto?.Invoke();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    float lives = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CrashObject.OnCrashedInto += DamagePlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void DamagePlayer()
+    {
+        if (lives <= 0)
+            Death();
+        lives--;
+    }
+
+    void Death()
+    {
+        Debug.Log("Death");
+        Application.Quit();
+    }
+}
