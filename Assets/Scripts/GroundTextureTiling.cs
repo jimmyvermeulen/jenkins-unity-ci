fd2f556df0088f74aedff04dca1072f0dd538fe7
Unknown <vermeulen.jimmy@gmail.com>
2019-12-16 03:27:46 +0100

    Add ground material and texture tiling

---
+++ Assets/Scripts/GroundTextureTiling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTextureTiling : MonoBehaviour
+{
+    [SerializeField]
+    float horizontalTiling;
+    [SerializeField]
+    float verticalTiling;
+    Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScaleTexture();
+    }
+    
+    void ScaleTexture()
+    {
+        material = GetComponent<MeshRenderer>().material;
+        material.mainTextureScale = new Vector2(horizontalTiling * transform.lossyScale.x * 10, 
+                                                verticalTiling * transform.lossyScale.z * 10);
+    }
+}
