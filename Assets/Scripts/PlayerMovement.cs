e8f0b2d9b6ca590e43ca769e4c164987542cd569
Unknown <vermeulen.jimmy@gmail.com>
2019-12-16 02:44:47 +0100

    Add forward moving player object and ground plane

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveForward(speed * Time.deltaTime);
+    }
+
+    void MoveForward(float distance)
+    {
+        transform.position += transform.forward * distance;
+    }
+}
