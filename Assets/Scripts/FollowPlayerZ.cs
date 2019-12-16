f826a7a61655a38afaf2caeb4af5a0e5564ab9b3
Unknown <vermeulen.jimmy@gmail.com>
2019-12-16 03:37:55 +0100

    Add camera only following the player in the z axis

---
+++ Assets/Scripts/FollowPlayerZ.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerZ : MonoBehaviour
+{
+    Vector3 startOffset;
+    [SerializeField]
+    Transform playerTransform;
+
+    private void Start()
+    {
+        startOffset = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, startOffset.y, playerTransform.position.z + startOffset.z);
+    }
+}
