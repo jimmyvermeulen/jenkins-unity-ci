f044ef27ba9d3d057c66c297ef7e17b76b14fc5b
Unknown <vermeulen.jimmy@gmail.com>
2019-12-16 04:03:18 +0100

    Add score pickup and scoring system

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        UpdateUI();
+        ScorePickup.OnPickedUp += AddPoint;
+    }
+
+    void AddPoint()
+    {
+        score++;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreText.text = $"Score: {score}";
+    }
+}
